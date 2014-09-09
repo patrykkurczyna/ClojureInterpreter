9f40e556e7b3da53e6f13c0686676fcb6639c9bb
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2014-09-09 18:48:34 +0200

    BuiltinFunctions changed to Clojure specific

--- BuiltInFunctions.py
+++ BuiltInFunctions.py
@@ -11,54 +11,54 @@ builtIns = {
 	'>'  : lambda x: evalBoolExpr(x, '>'),
 	'<=' : lambda x: evalBoolExpr(x, '<='),
 	'>=' : lambda x: evalBoolExpr(x, '>='),
-	'eq' : lambda x: evalEq(x),
+	'=' : lambda x: evalEq(x),
 	
 	'not': lambda x: evalNot(x),
 	'and' : lambda x: evalAnd(x),
 	'or' : lambda x: evalOr(x),
 
-	'car'    : lambda x: x[0][0],
-	'cdr'    : lambda x: x[0][1:],
-	'subseq' : lambda x: x[2][x[0]:x[1]],
+	'first'    : lambda x: x[0][0],
+	'rest'    : lambda x: x[0][1:],
+	'.substring' : lambda x: x[2][x[0]:x[1]],
 	'nth'    : lambda x: x[1][x[0]],
-	'length' : lambda x: evalLength(x[0]),
+	'.length' : lambda x: evalLength(x[0]),
 
 	'print'  : lambda x: evalPrint(x[0]),
-	'setq'   : lambda x: evalSetq(x),
+	'def'   : lambda x: evalSetq(x),
 
-	'if'	 : lambda x: (x[2], x[1])[x[0] != "NIL"]
+	'if'	 : lambda x: (x[2], x[1])[x[0] != "false"]
 }
 
 def evalBoolExpr(x, sign):
 	for i in range(len(x)-1):
 		if not eval(str(x[i]) + " " + str(sign) + " " + str(x[i+1])):
-			return "NIL"
-	return "T"
+			return "false"
+	return "true"
 
 def evalEq(x):
 	if len(x) != 2 or x[0]==None or x[1]==None:
 		raise FunctionNotFound
 	if x[0] == x[1]:
-		return "T"
+		return "true"
 	else:
-		return "NIL"
+		return "false"
 
 def evalNot(x):
 	if len(x) != 1 or x[0]==None:
 		raise FunctionNotFound
-	return "T" if x[0] == "NIL" else "NIL"
+	return "true" if x[0] == "false" else "false"
 
 def evalAnd(x):
 	for i in range(len(x)):
-		if x[i] == "NIL":
-			return "NIL"
+		if x[i] == "false":
+			return "false"
 	return x[len(x)-1]
 
 def evalOr(x):
 	for i in range(len(x)):
-		if x[i] != "NIL":
+		if x[i] != "false":
 			return x[i]
-	return "NIL"
+	return "false"
 
 def evalLength(x):
 	if(type(x) == str):
